a6e445b5d909c2a17b29d3c454fccc40e0d7cd44
Ryan Mullen <rp.mullen50@gmail.com>
2022-04-08 22:16:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # packages
-import sys
 import numpy as np
 from matplotlib import pyplot as plt, animation
 from PIL import Image
 from sklearn.preprocessing import normalize
+import sys
 
-# load image data
 image_name = sys.argv[1]
 image = Image.open(image_name)
+size = image.size
+
 img_array = np.flipud(np.array(image)[:,:,0])
 
 img_x = len(img_array[:,0])
@@ -46,28 +47,26 @@ class Wavefunction:
 
         self.G = 40
 
-        self.dt = 0.5
-        self.t = 0
-        
+        self.dt = 1.5
+
         self.Vhat = -np.fft.fftn(4.0*np.pi*self.G*(np.abs(self.psi)**2-1.0)) / (self.kSq + (self.kSq==0))
         self.V = np.real(np.fft.ifftn(self.Vhat))
 
-      
+        self.t = 0
+
     def evolve(self):
-    
-        # 1/2 step in xy-space
+        # 1/2 step
         self.psi = np.exp(-1.j*self.dt/2.0*self.V) * self.psi
 
-        #full-step in k-space
+        #drift
         self.psihat = np.fft.fftn(self.psi)
         self.psihat = np.exp(self.dt * (-1.j*self.kSq/2.)) * self.psihat
         self.psi = np.fft.ifftn(self.psihat)
 
-        # update potential
         self.Vhat = -np.fft.fftn(4.0*np.pi*self.G*(np.abs(self.psi)**2-1.0)) / (self.kSq + (self.kSq==0))
         self.V = np.real(np.fft.ifftn(self.Vhat))
 
-        # 1/2 step in xy-space
+        # 1/2 kick
         self.psi = np.exp(-1.j*self.dt/2.0*self.V) * self.psi
 
         # update time
@@ -78,7 +77,6 @@ class Wavefunction:
 
 psi = Wavefunction(psi0)
 
-# plotting
 fig, ax = plt.subplots()
 psi_ax = plt.pcolormesh(psi.rho(), cmap='inferno')
 
@@ -88,5 +86,6 @@ def animate(i):
     plt.title(r"$\rho = |\psi|^{2}$")
     psi.evolve()
 
+filename = sys.argv[1].split(".")[0] + ".mp4"
 anim = animation.FuncAnimation(fig, animate, interval = 50, frames = 250)
-anim.save("main.mp4")
+anim.save(filename)
